2bcc29465e0be8f2687dfe5e1079a0693e7d59a1
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-08 20:58:02 +0300

    called logic to run the DB

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require("./app");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+const connectMongoDB = require("./db/mongoDB");
 
 app.listen(config.PORT, () => {
+    connectMongoDB();
     logger.info(`Server ready and running on port: ${config.PORT}`)
 });
