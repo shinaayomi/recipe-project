ae48746136f7ea8f2fcbad26bfbb9791654ed289
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-08 20:57:15 +0300

    DB: logic to connect to mongoDB databse

---
+++ db/mongoDB.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const logger = require("../utils/logger");
+const config = require("../utils/config");
+
+const connectMongoDB = async () => {
+    try {
+        logger.info("Connecting to MongoDB, please wait...");
+
+        const connectDB = await mongoose.connect(config.MONGODB_URI);
+        logger.info(`MongoDB Database connected successfully on: ${connectDB.connection.host}`)
+    } catch (error) {
+        logger.error(`Error connecting to MongoDB Database: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+module.exports = connectMongoDB;
