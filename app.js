a0c687c734274db728098645bf26ec788953182a
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-08 20:57:46 +0300

    imported additional middlewares

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(express.json({ limit: "5mb" }));
+app.use(morgan("dev"));
+
+const indexRoutes = require("./routes");
+
+app.use("/api/v1", indexRoutes);
 
 module.exports = app;
