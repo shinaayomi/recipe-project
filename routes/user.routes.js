72b046fc669acca209ea78f050696da4de3126c7
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-12 20:18:02 +0300

    appended validation middleware rule to route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,9 @@
 const { signup, login } = require("../controllers/user.controller");
 const express = require("express");
-const { validateLogin } = require("../validators/auth");
+const { validateLogin, validateSignUp } = require("../validators/auth");
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/signup", validateSignUp, signup);
 router.post("/login", validateLogin, login);
 
 module.exports = router;
