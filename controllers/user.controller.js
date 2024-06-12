e49014c412df586ee04fac5363e42eefc5c52b96
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-06-12 15:21:51 +0300

    controller logic to login user and set token as cookie

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 const validator = require("express-validator");
 const User = require("../model/user.model");
 const logger = require("../utils/logger");
+const generateTokenAndSetCookie = require("../utils/generateToken");
 
 // @desc register new user controller logic
 const signup = async (req, res) => {
@@ -39,8 +40,39 @@ const signup = async (req, res) => {
         logger.error("Error Occured in signup controller", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
-}
+};
+
+// @desc login user controller logic
+const login = async (req, res) => {
+    try {
+        const { name, password } = req.body;
+
+        const user = await User.findOne({ name });
+
+        // check if password is correct with the one in DB
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({ status: "error", error: "Invalid username or password" });
+        };
+
+        // generate token as cookie
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            status: "success",
+            message: "Login successful",
+            data: {
+                _id: user._id,
+                name: user.name,
+            }
+        });
+
+    } catch (error) {
+        logger.error("Error occured in login controller", error);
+        res.status(500).json({ status: "error", error: "Internal Server Error" });
+    }
+};
 
 module.exports = {
-    signup
+    signup, login
 };
