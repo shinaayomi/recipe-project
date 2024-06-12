130f50182529269ebeca172e905a488214730a0c
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-06-12 15:21:59 +0300

    route to login user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
-const { signup } = require("../controllers/user.controller");
+const { signup, login } = require("../controllers/user.controller");
 const express = require("express");
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
