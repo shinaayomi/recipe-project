4bf4fa79e67f23541f29ee81c8a7eb7125eb50e7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-06-12 15:40:20 +0300

    express-validator middleware logic usage

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const { signup, login } = require("../controllers/user.controller");
 const express = require("express");
+const { validateLogin } = require("../validators/auth");
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+router.post("/login", validateLogin, login);
 
 module.exports = router;
