fea991d6a39df35837720ff1fa10ebe4fbf5c485
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-12 20:16:51 +0300

    update: refactor validation logic

--- validators/auth.js
+++ validators/auth.js
@@ -1,25 +1,35 @@
 const { check, validationResult } = require("express-validator");
 
-const validateLogin = [
-    check('name')
-        .notEmpty().withMessage("Name is required")
-        .isLength({ min: 4 }).withMessage("Name must be atleast 4 characters long!"),
-    check('password')
-        .notEmpty().withMessage("Password is required")
-        .isLength({ min: 6 }).withMessage("Password must be atleast 6 characters long!"),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            return res.status(400).json({
-                status: "error",
-                error: "Validation Failed",
-                details: errors.array()
-            });
-        }
-        next();
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const errorObj = {};
+
+        errors.array().forEach(e => {
+            errorObj[e.path] = (errorObj[e.path] || []).concat([e.msg]);
+        });
+
+        return res.status(400).json({
+            status: "error",
+            message: "Validation Error",
+            data: errorObj,
+        })
     }
-]
+};
+
+const validateSignUp = [
+    check('fullName').notEmpty().withMessage("Fullname is required"),
+    check('email').notEmpty().withMessage("Email is required"),
+    check('password').notEmpty().withMessage("Password is required").isLength({ min: 6, max: 20 }).withMessage("Password length must be atleast 6 characters long"),
+    validate,
+];
+
+const validateLogin = [
+    check('email').notEmpty().withMessage("Email is required"),
+    check('password').notEmpty().withMessage("Password is required").isLength({ min: 6, max: 20 }).withMessage("Password length must be atleast 6 characters long"),
+    validate
+];
 
 module.exports = {
-    validateLogin
+    validateLogin, validateSignUp
 };
