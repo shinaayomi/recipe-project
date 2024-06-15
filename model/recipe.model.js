5392808b600c36be931f9b3c76370aac9f3c50e4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-06-15 16:12:06 +0300

    recipe schema created

---
+++ model/recipe.model.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+
+const ingridientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    quantity: {
+        type: String,
+        required: true
+    },
+});
+
+const instructionSchema = new mongoose.Schema({
+    stepNumber: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+});
+
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    ingridients: [ingridientSchema],
+    instructions: [instructionSchema],
+    prepTime: {
+        type: Number, // in minutes
+        required: true,
+    },
+    cookTime: {
+        type: Number, // in minutes
+        required: true,
+    },
+    servings: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        enum: ["Breakfast", "Lunch", "Dinner", "Snack", "Dessert", "Drink"],
+        required: true
+    },
+    cuisine: {
+        type: String,
+        required: true,
+    },
+    difficulty: {
+        type: String,
+        enum: ["Easy", "Medium", "Hard"],
+        required: true
+    },
+    image: {
+        type: String,
+        default: "",
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+},
+{ timestamps: { createdAt: "Date_Created", updatedAt: "Date_Updated" }, 
+}
+);
+
+module.exports = mongoose.model("Recipe", recipeSchema);
