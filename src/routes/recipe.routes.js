b51b79877de114b11c7867ffdee8c0dda40c9ed3
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-19 19:45:52 +0300

    middleware to restrict only owner to modify recipes

--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const auth = require("../middleware/auth.middleware");
 const { createRecipe, getAllRecipes, getRecipeById, updateRecipeById, deleteRecipeById } = require("../controllers/recipe.controller");
+const restrictToOwner = require("../middleware/restrict.middleware");
 const router = express.Router();
 
 router.post("/", auth, createRecipe);
 router.get("/", auth, getAllRecipes);
 router.get("/:recipeId", auth, getRecipeById);
-router.put("/:recipeId", auth, updateRecipeById);
-router.delete("/:recipeId", auth, deleteRecipeById)
+router.put("/:recipeId", auth, restrictToOwner, updateRecipeById);
+router.delete("/:recipeId", auth, restrictToOwner, deleteRecipeById)
 
 module.exports = router;
