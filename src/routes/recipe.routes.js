d7bbe013b917e929a5d7247e8cf698b0803cc540
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-19 19:17:28 +0300

    update recipe by Id route elogic

--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const auth = require("../middleware/auth.middleware");
-const { createRecipe, getAllRecipes, getRecipeById, updateRecipeById } = require("../controllers/recipe.controller");
+const { createRecipe, getAllRecipes, getRecipeById, updateRecipeById, deleteRecipeById } = require("../controllers/recipe.controller");
 const router = express.Router();
 
 router.post("/", auth, createRecipe);
 router.get("/", auth, getAllRecipes);
 router.get("/:recipeId", auth, getRecipeById);
-router.put("/:recipeId", updateRecipeById);
+router.put("/:recipeId", auth, updateRecipeById);
+router.delete("/:recipeId", auth, deleteRecipeById)
 
 module.exports = router;
